a9dde1f2e394d55da45ff4a8564d5e088c0ec2cd
Nazzid <68158086+Nazzid@users.noreply.github.com>
2021-04-21 23:35:24 +0700

    Update doge.js

--- plugins/doge.js
+++ plugins/doge.js
@@ -1,12 +1,34 @@
 const axios = require('axios')
+const { MessageType } = require('@adiwajshing/baileys')
 
+const getBuffer = async (url, options) => {
+    try {
+        options ? options : {}
+        const res = await axios({
+            method: "get",
+            url,
+            headers: {
+                'DNT': 1,
+                'Upgrade-Insecure-Request': 1
+            },
+            ...options,
+            responseType: 'arraybuffer'
+        })
+        return res.data
+    } catch (e) {
+        console.log(`Error : ${e}`)
+    }
+}
 let handler  = async (m, { conn }) => {
 	
 	axios.get('https://indodax.com/api/summaries')
 		.then(async res => {
 			doge = res.data.tickers.doge_idr
+			buffer = await getBuffer('')
 			text = `*Dogecoin ~ Nazzbot*\nHarga Terakhir : ${doge.last}\nTertinggi : ${doge.high}\nTerendah : ${doge.low}`
-			conn.reply(m.chat, teks, m)
+			conn.sendMessage(m.chat, buffer, MessageType.image, {caption: text})
+			
+			
 		})
 }
 
